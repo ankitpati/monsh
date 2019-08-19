c43161cdf76cefd099982ad4f17c1daa708108d2
Ankit Pati <contact@ankitpati.in>
2019-08-19 11:52:13 +0530

    simple plugin to check nagios is working

---
+++ plugins/nagios.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Run this through Nagios and check the output through the web interface.
+# Running this in the terminal doesn’t do much.
+
+echo 'OK - Nagios correctly configured and running'
